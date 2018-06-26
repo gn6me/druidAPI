3ee10450dba2dc0d62dbeb015c33909cc5481b16
gn6me <mrt.sleeper@gmail.com>
2018-06-26 16:19:52 -0400

    Basic api finished

--- app/routes/druid_routes.js
+++ app/routes/druid_routes.js
@@ -1,9 +1,21 @@
 // routes/note_routes.js
 
+var ObjectID = require('mongodb').ObjectID;
+
 module.exports = function(app, db) {
 
 	// lookup druid
 	app.get('/api/druids/:id', (req, res) => {
+
+		const id = req.params.id;
+		const details = { '_id': new ObjectID(id) };
+		db.collection('druids').findOne(details, (err, item) => {
+			if (err) {
+				res.send({ 'error': 'An error has ocurred' });
+			} else {
+				res.send(item);
+			}
+		});
 
 	});
 
@@ -18,6 +30,37 @@ module.exports = function(app, db) {
    				res.send(result.ops[0]);
    			}
    		});
+
+  	});
+
+  	// delete a druid
+  	app.delete('/api/druids/:id', (req, res) => {
+
+  		const id = req.params.id;
+  		const details = { '_id': new ObjectID(id)  };
+  		db.collection('druids').remove(details, (err, item) => {
+  			if (err) {
+  				res.send({ 'error': 'An error has ocurred' });
+  			} else {
+  				res.send('Druid ' + id + ' deleted!');
+  			}
+  		});
+
+  	});
+
+  	//update a druid
+  	app.put('/api/druids/:id', (req, res) => {
+
+  		const id = req.params.id;
+  		const details = { '_id': new ObjectID(id) };
+  		const druid = { text: req.body.body, title: req.body.title };
+  		db.collection('druids').update(details, druid, (err, result) => {
+  			if (err) {
+  				res.send({ 'error': 'An error has ocurred' });
+  			} else {
+  				res.send(druid);
+  			}
+  		});
 
   	});
 };
